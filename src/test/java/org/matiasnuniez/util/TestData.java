269cbdf92b6ff3869a0744536c9e79fe2d7519ea
MatiasNuniez <matiasnuniez192@gmail.com>
2026-03-30 13:41:57 -0300

    feat(util): externalize test data constants for Demoblaze purchase

--- src/test/java/org/matiasnuniez/util/TestData.java
+++ src/test/java/org/matiasnuniez/util/TestData.java
@@ -2,8 +2,20 @@ package org.matiasnuniez.util;
 
 public final class TestData {
 
-    // Insertar aquí las constantes de datos de prueba, por ejemplo:
-    // public static final String YAHOO_URL = "https://espanol.yahoo.com/";
+    public static final String DEMOBLAZE_URL = "https://www.demoblaze.com/";
+
+    public static final String PRODUCTO_1 = "Samsung galaxy s6";
+    public static final String PRODUCTO_2 = "Nokia lumia 1520";
+    public static final String[] PRODUCTOS = {PRODUCTO_1, PRODUCTO_2};
+
+    public static final String COMPRADOR_NOMBRE = "John Doe";
+    public static final String COMPRADOR_PAIS = "USA";
+    public static final String COMPRADOR_CIUDAD = "New York";
+    public static final String COMPRADOR_TARJETA = "4111111111111111";
+    public static final String COMPRADOR_MES = "12";
+    public static final String COMPRADOR_ANIO = "2025";
+
+    public static final String MENSAJE_COMPRA_EXITOSA = "Thank you for your purchase!";
 
     private TestData() {
     }
