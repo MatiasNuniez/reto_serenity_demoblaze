80b98aa9e32647006ab23bfb45504be1260c4394
MatiasNuniez <matiasnuniez192@gmail.com>
2026-03-30 12:44:37 -0300

    initial commit

---
+++ src/test/java/org/matiasnuniez/util/TestData.java
@@ -0,0 +1,10 @@
+package org.matiasnuniez.util;
+
+public final class TestData {
+
+    // Insertar aquí las constantes de datos de prueba, por ejemplo:
+    // public static final String YAHOO_URL = "https://espanol.yahoo.com/";
+
+    private TestData() {
+    }
+}
